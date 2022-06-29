fe94f1d6be68da19f53c3b8b122369a181a8fb5b
RHONG <rocky55737@gmail.com>
2022-06-29 18:34:10 +0900

    1.8 bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhong <rhong@student.42Seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/10 16:57:50 by rhong             #+#    #+#             */
-/*   Updated: 2022/06/29 18:28:43 by rhong            ###   ########.fr       */
+/*   Created: 2022/06/29 18:22:48 by rhong             #+#    #+#             */
+/*   Updated: 2022/06/29 18:28:54 by rhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
+
+# define OPEN_MAX 65534
 
 size_t	my_strlen(char const *str);
 char	*get_next_line(int fd);
